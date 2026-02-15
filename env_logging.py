202725c1c4fdf1e4edf7fc8e0b167ceb88c55301
Jett Brister <jett@brister.dev>
2026-02-15 00:31:21 -0700

    fixes

--- env_logging.py
+++ env_logging.py
@@ -9,10 +9,14 @@ print('Warming Up SGP30...')
 time.sleep(16)
 
 LOG_FILE = 'environmental_log.csv'
+BASELINE_INTERVAL = 6 * 60 * 60  # 6 hours
 
 with open(LOG_FILE, 'w', newline='') as file:
 	writer = csv.writer(file)
 	writer.writerow(['timestamp','temp_c','pressure','humidity','eco2','tvoc'])
+
+sgp.load_baseline()
+last_baseline = time.time()
 
 while True:
 	try:
@@ -23,6 +27,9 @@ while True:
 			writer = csv.writer(file)
 			writer.writerow([datetime.today(), temp_c, pressure, humidity, eco2, tvoc])
 		print(datetime.today(), temp_c, pressure, humidity, eco2, tvoc)
+		if time.time() - last_baseline > BASELINE_INTERVAL:
+			sgp.save_baseline()
+			last_baseline = time.time()
 		time.sleep(1)
 	except KeyboardInterrupt:
 		print('Program stopped')
