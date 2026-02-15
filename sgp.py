98b826dc6667f77c0c8a0d031eb84e23ac978295
Jett Brister <jett@brister.dev>
2026-02-15 00:05:54 -0700

    added baseline persistence feature

--- sgp.py
+++ sgp.py
@@ -1,10 +1,15 @@
 # Required Libraries
 import time
+import json
+import os
 # Uses ADD PACKAGE NAME
 from sgp30 import SGP30
 
 # Initialize sensor
 sgp30 = SGP30(bus=1)
+
+# Baseline File:
+BASELINE_FILE = 'sgp30_baseline.json'
 
 # Functions for reading data:
 
@@ -24,7 +29,7 @@ def raw_values():
 	'''Returns [Raw H2 Signal, Raw Ethanol Signal].'''
 	return [sgp30.H2, sgp30.Ethanol]
 
-def baseline():
+def read_baseline():
 	'''Returns current baseline [eCO2, tVOC].'''
 	return [sgp30.baseline_eCO2, sgp30.baseline_TVOC]
 
@@ -37,3 +42,20 @@ def set_baseline(eCO2_baseline, tVOC_baseline):
 def set_rh(humidity, temp_C):
 	'''Sets relative humidity for IAQ profile. Pass (humidity, temp C)'''
 	sgp30.set_iaq_relative_humidity(temp_C, humidity)
+
+# Baseline Persistence
+
+def save_baseline():
+	'''Stores baseline values in json'''
+	baseline = read_baseline()
+	data = {'time':time.time(), 'eco2':baseline[0], 'tvoc':baseline[1]}
+	with open(BASELINE_FILE, 'w') as file:
+		json.dump(data, file)
+
+def load_baseline()
+	'''Loads baseline values from json'''
+	if not os.path.exists(BASELINE_FILE):
+		return False
+	with open(BASELINE_FILE, 'r') as file:
+		data = json.load(file)
+	set_baseline(data['eco2'], data['tvoc'])
