8a86fa82791d73683927c3f25f1a29f3cf927dc9
Jett Brister <jett@brister.dev>
2026-02-19 12:57:53 -0700

    log pull

--- env_logging.py
+++ env_logging.py
@@ -42,8 +42,8 @@ while True:
 				writer = csv.writer(file)
 				writer.writerow([datetime.now(), temp_c, pressure, humidity, eco2, tvoc, raw_H2, raw_Eth, baseline_eco2, baseline_tvoc])
 			current_min = datetime.now().minute
-			print('Data Point Logged')
-		print(f"{temp_c:.2f}", f"{pressure:.2f}", f"{humidity:.2f}", eco2, tvoc, raw_H2, raw_Eth, baseline_eco2, baseline_tvoc)
+#			print('Data Point Logged')
+#		print(f"{temp_c:.2f}", f"{pressure:.2f}", f"{humidity:.2f}", eco2, tvoc, raw_H2, raw_Eth, baseline_eco2, baseline_tvoc)
 		if time.time() - last_baseline > BASELINE_INTERVAL:
 			sgp.save_baseline()
 			last_baseline = time.time()
